2621d1c0ccecab5dcf57c836b31dff167dc95910
Eric Drake <puturosule@gmail.com>
2022-11-29 08:43:34 -0700

    added the hasBalancedParenthesis function

--- balancedParens.js
+++ balancedParens.js
@@ -5,3 +5,14 @@ let sample3 = "This is () also ) unbalanced."
 let sample4 = "Balanced."
 
 // Write your solution below:
+const hasBalancedParenthesis = sample => {
+    let openCount = [...sample.matchAll(/\(/g)].length;
+    let closeCount = [...sample.matchAll(/\)/g)].length;
+    return (sample.indexOf('(') <= sample.indexOf(')')) && openCount === closeCount;
+}
+
+console.log(hasBalancedParenthesis(sample1));
+console.log(hasBalancedParenthesis(sample2));
+console.log(hasBalancedParenthesis(sample3));
+console.log(hasBalancedParenthesis(sample4));
+//console.log(hasBalancedParenthesis(`This is () also ) unbalanced.`));
